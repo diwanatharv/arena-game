b3319b8c5364d837e000b06453a497916a6363af
diwanatharv <atharvdiwan26@gmail.com>
2023-12-28 00:04:05 +0530

    finishing the model folder

--- model/die.go
+++ model/die.go
@@ -1,1 +1,15 @@
 package model
+
+import (
+	"math/rand"
+	"time"
+)
+
+type Die struct {
+	Sides int
+}
+
+func (d *Die) Roll() int {
+	rand.Seed(time.Now().UnixNano())
+	return rand.Intn(d.Sides) + 1
+}
--- model/player.go
+++ model/player.go
@@ -1,1 +1,23 @@
 package model
+
+type Player struct {
+	Health, Strength, Attack int
+}
+
+func (p *Player) Attacking() int {
+	attackDie := Die{6}
+	roll := attackDie.Roll()
+	damage := p.Attack * roll
+	return damage
+}
+
+func (p *Player) Defend(damage int) {
+	defendDie := Die{6}
+	roll := defendDie.Roll()
+	defendedDamage := p.Strength * roll
+	p.Health -= damage - defendedDamage
+}
+
+func (p *Player) IsAlive() bool {
+	return p.Health > 0
+}
