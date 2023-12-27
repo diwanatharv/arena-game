df448106a6662ecbbaf6f2dfec72600dc65db7f7
diwanatharv <atharvdiwan26@gmail.com>
2023-12-28 00:10:56 +0530

    finishing all the changes

--- main.go
+++ main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"awesomeProject58/model"
+	"awesomeProject58/service"
+)
 
+func main() {
+	player1 := model.Player{Health: 50, Strength: 5, Attack: 10}
+	player2 := model.Player{Health: 100, Strength: 10, Attack: 5}
+	arena := service.Arena{Player1: player1, Player2: player2}
+	arena.StartMatch()
+	arena.PrintMatchResults()
 }
--- service/arena.go
+++ service/arena.go
@@ -1,1 +1,59 @@
 package service
+
+import (
+	"awesomeProject58/model"
+	"fmt"
+)
+
+type Arena struct {
+	Player1 model.Player
+	Player2 model.Player
+}
+
+func (a *Arena) StartMatch() {
+	// Determine starting player
+	currentPlayer := a.getStartingPlayer()
+
+	// Match loop
+	for {
+		damage := currentPlayer.Attacking() // Assuming Attack() is a method on Player struct
+		opponent := a.getOpponent(currentPlayer)
+		opponent.Defend(damage)
+
+		// Print round results (using indices if Name field is not available)
+		fmt.Printf("Player %d attacks Player %d for %d damage.\n", 1, 2, damage)
+		fmt.Printf("Player %d's health is now %d.\n", 2, opponent.Health)
+
+		if !opponent.IsAlive() {
+			break
+		}
+
+		// Switch players
+		currentPlayer = opponent
+	}
+}
+
+func (a *Arena) PrintMatchResults() {
+	if a.Player1.IsAlive() {
+		fmt.Printf("Player 1 wins! Player 2 has been defeated.\n")
+	} else {
+		fmt.Printf("Player 2 wins! Player 1 has been defeated.\n")
+	}
+	fmt.Printf("Player 1's final health: %d\n", a.Player1.Health)
+	fmt.Printf("Player 2's final health: %d\n", a.Player2.Health)
+}
+
+func (a *Arena) getStartingPlayer() *model.Player {
+	if a.Player2.Health < a.Player1.Health {
+		return &a.Player2
+	}
+	return &a.Player1
+}
+
+func (a *Arena) getOpponent(player *model.Player) *model.Player {
+	if player == &a.Player1 {
+		return &a.Player2
+	} else {
+		return &a.Player1
+	}
+}
